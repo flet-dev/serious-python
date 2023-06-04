3254260a25c34a4966630c5a891117de0c30b75b
Feodor Fitsner <feodor@appveyor.com>
2023-06-03 21:09:00 -0700

    draft of package CLI

--- bin/package.dart
+++ bin/package.dart
@@ -1,40 +1,117 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:archive/archive_io.dart';
 import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
+import 'package:toml/toml.dart';
 
-void main(List<String> args) {
-  stdout.writeln("Hello from command line");
-  // Parsing arguments
-  final parser = ArgParser();
-  parser.addOption("src");
-  parser.addFlag("help", abbr: "h");
-  parser.addFlag('emojis', abbr: 'e', negatable: false);
-  final argResults = parser.parse(args);
-  if (argResults["help"]) {
-    stdout.writeln(("Usage:"));
-    exit(0);
+void main(List<String> arguments) async {
+  final parser = ArgParser()
+    ..addOption('dest', abbr: 'd', help: 'destination archive path and name');
+  //..addPositional('source', help: 'source directory path');
+
+  final args = parser.parse(arguments);
+
+  if (args.rest.isEmpty) {
+    stdout.writeln("Usage:");
+    stdout.writeln(
+        "  flutter pub run serious_python:package [options] <source_dir>\n");
+    stdout.writeln("Options:");
+    stdout.writeln(parser.usage);
+    exit(1);
   }
-  var srcDir = argResults["src"];
-  stdout.writeln("Source dir: $srcDir");
 
-  /*
+  final sourceDir = Directory(args['source']);
+  final dest = File(args['dest']);
 
-  Swift module TODO:
-  - run from .pyc
+  if (!sourceDir.existsSync()) {
+    stdout.writeln('Source directory does not exist');
+    exit(2);
+  }
 
-  // installing python modules
-  // pip install --isolated --upgrade --target /path/to/__pypackages__ -r ___requirements.txt
+  final tempDir = Directory.systemTemp.createTempSync('copy_and_zip');
+  copyDirectory(sourceDir, tempDir);
 
-        "CC": "/bin/false",
-        "CXX": "/bin/false",
-        "PYTHONPATH": ctx.site_packages_dir,
-        "PYTHONOPTIMIZE": "2",
+  List<String>? dependencies;
+  final pyprojectFile = File(path.join(tempDir.path, 'pyproject.toml'));
+  if (pyprojectFile.existsSync()) {
+    final content = await pyprojectFile.readAsString();
+    final document = TomlDocument.parse(content).toMap();
+    dependencies = List<String>.from(document['tool']['poetry']['dependencies']
+        .keys
+        .map(
+            (key) => '$key${document['tool']['poetry']['dependencies'][key]}'));
+  } else {
+    final requirementsFile = File(path.join(tempDir.path, 'requirements.txt'));
+    if (requirementsFile.existsSync()) {
+      dependencies = await requirementsFile.readAsLines();
+    }
+  }
 
-    After installation:
-    - remove all *.so, *.a files
-    - remove all __pycache__ dirs
-    - remove "bin" directory
-    - optionally: compile python files and remove .py:
-        python -m compileall -b ./app
-  */
+  if (dependencies != null) {
+    final pipProcess = await Process.start(
+        'pip3',
+        [
+          'install',
+          '--isolated',
+          '--upgrade',
+          '--target',
+          path.join(tempDir.path, '__pypackages__'),
+          ...dependencies
+        ],
+        environment: {
+          "CC": "/bin/false",
+          "CXX": "/bin/false",
+          "PYTHONPATH": tempDir.path,
+          "PYTHONOPTIMIZE": "2",
+        },
+        runInShell: true,
+        includeParentEnvironment: false);
+
+    await for (final line in pipProcess.stdout.transform(utf8.decoder)) {
+      stdout.write(line);
+    }
+
+    if (await pipProcess.exitCode != 0) {
+      stdout.write(await pipProcess.stderr.transform(utf8.decoder).join());
+      exit(1);
+    }
+  }
+
+  deleteDirectories(tempDir);
+
+  final encoder = ZipFileEncoder();
+  encoder.zipDirectory(tempDir, filename: dest.path);
+}
+
+void copyDirectory(Directory source, Directory destination) {
+  source.listSync().forEach((entity) {
+    if (entity is Directory) {
+      final newDirectory =
+          Directory(path.join(destination.path, path.basename(entity.path)));
+      newDirectory.createSync();
+      copyDirectory(entity.absolute, newDirectory);
+    } else if (entity is File) {
+      entity.copySync(path.join(destination.path, path.basename(entity.path)));
+    }
+  });
+}
+
+void deleteDirectories(Directory directory) {
+  directory.listSync().forEach((entity) {
+    if (entity is Directory) {
+      deleteDirectories(entity);
+    } else if (entity is File && entity.path.endsWith('.so')) {
+      entity.deleteSync();
+    }
+  });
+
+  directory.listSync().forEach((entity) {
+    if (entity is Directory && entity.path.endsWith('__pycache__')) {
+      entity.deleteSync(recursive: true);
+    } else if (entity is Directory && entity.path.endsWith('bin')) {
+      entity.deleteSync(recursive: true);
+    }
+  });
 }
