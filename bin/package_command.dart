e24ed6665e13cae78d4663041cb479654eda330e
Feodor Fitsner <feodor@appveyor.com>
2023-08-02 16:45:33 -0700

    Fix platform detection in `package` command

--- bin/package_command.dart
+++ bin/package_command.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:archive/archive_io.dart';
 import 'package:args/command_runner.dart';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 import 'package:toml/toml.dart';
@@ -261,7 +260,7 @@ class PackageCommand extends Command {
     }
 
     // Run the python executable
-    var pythonPath = defaultTargetPlatform == TargetPlatform.windows
+    var pythonPath = Platform.isWindows
         ? path.join(pythonDir.path, 'python', 'python.exe')
         : path.join(pythonDir.path, 'python', 'bin', 'python3');
     return await runExec(pythonPath, args, environment: environment);
