42ab597fbf94f9928e7010fc218ee5d549a04566
Feodor Fitsner <feodor@appveyor.com>
2023-09-29 12:10:42 -0700

    Added macOS plugin

---
+++ src/serious_python_macos/CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.4.0
+
+* macOS support added.
+
+## 0.3.0
+
+Initial version.
---
+++ src/serious_python_macos/lib/serious_python_macos.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:serious_python_platform_interface/serious_python_platform_interface.dart';
+
+/// An implementation of [SeriousPythonPlatform] that uses method channels.
+class SeriousPythonMacOS extends SeriousPythonPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('macos_plugin');
+
+  /// Registers this class as the default instance of [SeriousPythonPlatform]
+  static void registerWith() {
+    SeriousPythonPlatform.instance = SeriousPythonMacOS();
+  }
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+
+  @override
+  Future<String?> run(String appPath,
+      {List<String>? modulePaths,
+      Map<String, String>? environmentVariables,
+      bool? sync}) async {
+    // set environment variables
+    if (environmentVariables != null) {
+      for (var v in environmentVariables.entries) {
+        await methodChannel.invokeMethod<String>(
+            'setEnvironmentVariable', {'name': v.key, 'value': v.value});
+      }
+    }
+
+    // unpack python bundle
+    final nativeLibraryDir =
+        await methodChannel.invokeMethod<String>('getNativeLibraryDir');
+    debugPrint("getNativeLibraryDir: $nativeLibraryDir");
+
+    var bundlePath = "$nativeLibraryDir/libpythonbundle.so";
+
+    if (!await File(bundlePath).exists()) {
+      throw Exception("Python bundle not found: $bundlePath");
+    }
+
+    var pythonLibPath =
+        await extractFileZip(bundlePath, targetPath: "python_bundle");
+
+    debugPrint("pythonLibPath: $pythonLibPath");
+
+    runPythonProgramFFI(
+        sync ?? false, "libpython3.10.so", pythonLibPath, appPath, [
+      ...?modulePaths,
+      "$pythonLibPath/modules",
+      "$pythonLibPath/site-packages",
+      "$pythonLibPath/stdlib.zip"
+    ]);
+
+    return null;
+  }
+}
