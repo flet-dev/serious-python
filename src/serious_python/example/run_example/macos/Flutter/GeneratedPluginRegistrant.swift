865b3f86327a9f5cc07de68d1bf3f9067e9ae69e
Feodor Fitsner <feodor@appveyor.com>
2023-11-01 17:19:27 -0700

    Fix Android and iOS feature builds (#32)
    * Download python dists from jobs for non-release builds
    
    * copyBuildDist task
    
    * Enable all CI jobs
    
    * Cleanup
    
    * Re-enable pubspec.lock in apps
    
    * Deps updated

--- src/serious_python/example/run_example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/serious_python/example/run_example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import package_info_plus
 import path_provider_foundation
 import serious_python_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SeriousPythonPlugin.register(with: registry.registrar(forPlugin: "SeriousPythonPlugin"))
 }
