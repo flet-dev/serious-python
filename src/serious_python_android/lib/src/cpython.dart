9576d4cc285aa7e942d6ada9f49140bcb86e89f3
Feodor Fitsner <feodor@appveyor.com>
2024-05-02 14:08:50 -0700

    Some cleanup

--- src/serious_python_android/lib/src/cpython.dart
+++ src/serious_python_android/lib/src/cpython.dart
@@ -70,71 +70,37 @@ Future<String> runPythonProgramInIsolate(List<Object> arguments) async {
     final moduleNamePtr = programModuleName.toNativeUtf8();
     var modulePtr = cpython.PyImport_ImportModule(moduleNamePtr.cast<Char>());
     if (modulePtr == nullptr) {
-      final pType =
-          calloc.allocate<Pointer<PyObject>>(sizeOf<Pointer<PyObject>>());
-      final pValue =
-          calloc.allocate<Pointer<PyObject>>(sizeOf<Pointer<PyObject>>());
-      final pTrace =
-          calloc.allocate<Pointer<PyObject>>(sizeOf<Pointer<PyObject>>());
-
-      // https://stackoverflow.com/questions/1796510/accessing-a-python-traceback-from-the-c-api
-      //cpython.PyErr_Fetch(pType, pValue, pTrace);
+      // get error object
       var exPtr = cpython.PyErr_GetRaisedException();
 
-      final funcName = "__traceback__".toNativeUtf8();
-      final pFunc = cpython.PyObject_GetAttrString(exPtr, funcName.cast());
-      cpython.Py_DecRef(funcName.cast());
-
-      var pTraceback = cpython.PyObject_Str(pFunc);
-      var trace =
-          cpython.PyUnicode_AsUTF8(pTraceback).cast<Utf8>().toDartString();
-
-      // var pTypeStr = cpython.PyObject_Str(pType.value);
-      // var type = cpython.PyUnicode_AsUTF8(pTypeStr).cast<Utf8>().toDartString();
-
-      // var pValueStr = cpython.PyObject_Str(pValue.value);
-      // var value =
-      //     cpython.PyUnicode_AsUTF8(pValueStr).cast<Utf8>().toDartString();
-
-      // var pTraceStr = cpython.PyObject_Str(pTrace.value);
-      // var trace =
-      //     cpython.PyUnicode_AsUTF8(pTraceStr).cast<Utf8>().toDartString();
-
-      var pExStr = cpython.PyObject_Str(exPtr);
-      var ex = cpython.PyUnicode_AsUTF8(pExStr).cast<Utf8>().toDartString();
-
+      // use 'traceback' module to format exception
       final tracebackModuleNamePtr = "traceback".toNativeUtf8();
       var tracebackModulePtr =
           cpython.PyImport_ImportModule(tracebackModuleNamePtr.cast<Char>());
       cpython.Py_DecRef(tracebackModuleNamePtr.cast());
 
       if (tracebackModulePtr != nullptr) {
-        debugPrint("Traceback module loaded");
+        //debugPrint("Traceback module loaded");
 
         final formatFuncName = "format_exception".toNativeUtf8();
         final pFormatFunc = cpython.PyObject_GetAttrString(
             tracebackModulePtr, formatFuncName.cast());
+        cpython.Py_DecRef(tracebackModuleNamePtr.cast());
 
-        cpython.Py_DecRef(funcName.cast());
         if (pFormatFunc != nullptr &&
             cpython.PyCallable_Check(pFormatFunc) != 0) {
+          // call `traceback.format_exception()` method
           final pArgs = cpython.PyTuple_New(1);
           cpython.PyTuple_SetItem(pArgs, 0, exPtr);
-          // cpython.PyTuple_SetItem(pArgs, 1, pValue.value);
-          // cpython.PyTuple_SetItem(pArgs, 2, pTrace.value);
 
-          debugPrint("AFTER TUPLE");
+          // result is a list
+          var listPtr = cpython.PyObject_CallObject(pFormatFunc, pArgs);
 
-          var pythVal = cpython.PyObject_CallObject(pFormatFunc, pArgs);
-
-          debugPrint("AFTER CALL OBJECT");
-
-          //var pStackList = cpython.PyObject_Str(pythVal);
+          // get and combine list items
           var exLines = [];
-          var listSize = cpython.PyList_Size(pythVal);
-          debugPrint("List size: $listSize");
+          var listSize = cpython.PyList_Size(listPtr);
           for (var i = 0; i < listSize; i++) {
-            var itemObj = cpython.PyList_GetItem(pythVal, i);
+            var itemObj = cpython.PyList_GetItem(listPtr, i);
             var itemObjStr = cpython.PyObject_Str(itemObj);
             var s = cpython.PyUnicode_AsUTF8(itemObjStr)
                 .cast<Utf8>()
@@ -142,20 +108,13 @@ Future<String> runPythonProgramInIsolate(List<Object> arguments) async {
             exLines.add(s);
           }
           result = exLines.join("");
-          // var stack =
-          //     cpython.PyUnicode_AsUTF8(pStack).cast<Utf8>().toDartString();
-          // debugPrint("Stack: $stack");
         }
       } else {
         result = "Error loading traceback module";
       }
-
-      // debugPrint("Type: $type");
-      // debugPrint("Value: $value");
-      debugPrint("Trace: $trace");
-      debugPrint("Ex: $ex");
     }
     malloc.free(moduleNamePtr);
+    //malloc.free(modulePtr);
   }
 
   cpython.Py_Finalize();
