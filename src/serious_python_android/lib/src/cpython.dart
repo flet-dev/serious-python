107829980072f696cecda057402aa0f4672b816d
Feodor Fitsner <feodor@appveyor.com>
2024-05-02 13:18:10 -0700

    Format exception

--- src/serious_python_android/lib/src/cpython.dart
+++ src/serious_python_android/lib/src/cpython.dart
@@ -78,22 +78,68 @@ Future<String> runPythonProgramInIsolate(List<Object> arguments) async {
           calloc.allocate<Pointer<PyObject>>(sizeOf<Pointer<PyObject>>());
 
       // https://stackoverflow.com/questions/1796510/accessing-a-python-traceback-from-the-c-api
-      cpython.PyErr_Fetch(pType, pValue, pTrace);
+      //cpython.PyErr_Fetch(pType, pValue, pTrace);
+      var exPtr = cpython.PyErr_GetRaisedException();
 
-      var pTypeStr = cpython.PyObject_Str(pType.value);
-      var type = cpython.PyUnicode_AsUTF8(pTypeStr).cast<Utf8>().toDartString();
+      final funcName = "__traceback__".toNativeUtf8();
+      final pFunc = cpython.PyObject_GetAttrString(exPtr, funcName.cast());
+      cpython.Py_DecRef(funcName.cast());
 
-      var pValueStr = cpython.PyObject_Str(pValue.value);
-      var value =
-          cpython.PyUnicode_AsUTF8(pValueStr).cast<Utf8>().toDartString();
-
-      var pTraceStr = cpython.PyObject_Str(pTrace.value);
+      var pTraceback = cpython.PyObject_Str(pFunc);
       var trace =
-          cpython.PyUnicode_AsUTF8(pTraceStr).cast<Utf8>().toDartString();
+          cpython.PyUnicode_AsUTF8(pTraceback).cast<Utf8>().toDartString();
 
-      debugPrint("Type: $type");
-      debugPrint("Value: $value");
+      // var pTypeStr = cpython.PyObject_Str(pType.value);
+      // var type = cpython.PyUnicode_AsUTF8(pTypeStr).cast<Utf8>().toDartString();
+
+      // var pValueStr = cpython.PyObject_Str(pValue.value);
+      // var value =
+      //     cpython.PyUnicode_AsUTF8(pValueStr).cast<Utf8>().toDartString();
+
+      // var pTraceStr = cpython.PyObject_Str(pTrace.value);
+      // var trace =
+      //     cpython.PyUnicode_AsUTF8(pTraceStr).cast<Utf8>().toDartString();
+
+      var pExStr = cpython.PyObject_Str(exPtr);
+      var ex = cpython.PyUnicode_AsUTF8(pExStr).cast<Utf8>().toDartString();
+
+      final tracebackModuleNamePtr = "traceback".toNativeUtf8();
+      var tracebackModulePtr =
+          cpython.PyImport_ImportModule(tracebackModuleNamePtr.cast<Char>());
+      cpython.Py_DecRef(tracebackModuleNamePtr.cast());
+
+      if (tracebackModulePtr != nullptr) {
+        debugPrint("Traceback module loaded");
+
+        final formatFuncName = "format_exception".toNativeUtf8();
+        final pFormatFunc = cpython.PyObject_GetAttrString(
+            tracebackModulePtr, formatFuncName.cast());
+
+        cpython.Py_DecRef(funcName.cast());
+        if (pFormatFunc != nullptr &&
+            cpython.PyCallable_Check(pFormatFunc) != 0) {
+          final pArgs = cpython.PyTuple_New(1);
+          cpython.PyTuple_SetItem(pArgs, 0, exPtr);
+          // cpython.PyTuple_SetItem(pArgs, 1, pValue.value);
+          // cpython.PyTuple_SetItem(pArgs, 2, pTrace.value);
+
+          debugPrint("AFTER TUPLE");
+
+          var pythVal = cpython.PyObject_CallObject(pFormatFunc, pArgs);
+
+          debugPrint("AFTER CALL OBJECT");
+
+          var pStack = cpython.PyObject_Str(pythVal);
+          var stack =
+              cpython.PyUnicode_AsUTF8(pStack).cast<Utf8>().toDartString();
+          debugPrint("Stack: $stack");
+        }
+      }
+
+      // debugPrint("Type: $type");
+      // debugPrint("Value: $value");
       debugPrint("Trace: $trace");
+      debugPrint("Ex: $ex");
 
       result = "Error running Python program";
     }
