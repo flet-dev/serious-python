f23288164100a0ecaadcde027907c9871ad7d26e
Feodor Fitsner <feodor@appveyor.com>
2025-01-05 17:32:09 -0800

    Added com.flet.serious_python_android.PythonActivity

--- src/serious_python_android/android/src/main/java/com/flet/serious_python_android/AndroidPlugin.java
+++ src/serious_python_android/android/src/main/java/com/flet/serious_python_android/AndroidPlugin.java
@@ -5,7 +5,6 @@ import android.content.ContextWrapper;
 import androidx.annotation.NonNull;
 import android.system.Os;
 import android.content.Intent;
-import android.app.Activity;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -15,13 +14,14 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
+import com.flet.serious_python_android.PythonActivity;
+
 /** AndroidPlugin */
 public class AndroidPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
   public static final String MAIN_ACTIVITY_HOST_CLASS_NAME = "MAIN_ACTIVITY_HOST_CLASS_NAME";
   public static final String MAIN_ACTIVITY_CLASS_NAME = "MAIN_ACTIVITY_CLASS_NAME";
-  public static final String NATIVE_LIBRARY_DIR = "NATIVE_LIBRARY_DIR";
-  public static Activity mActivity = null;
+  public static final String ANDROID_NATIVE_LIBRARY_DIR = "ANDROID_NATIVE_LIBRARY_DIR";
 
   /// The MethodChannel that will the communication between Flutter and native
   /// Android
@@ -39,7 +39,7 @@ public class AndroidPlugin implements FlutterPlugin, MethodCallHandler, Activity
     channel.setMethodCallHandler(this);
     this.context = flutterPluginBinding.getApplicationContext();
     try {
-      Os.setenv(NATIVE_LIBRARY_DIR, new ContextWrapper(this.context).getApplicationInfo().nativeLibraryDir, true);
+      Os.setenv(ANDROID_NATIVE_LIBRARY_DIR, new ContextWrapper(this.context).getApplicationInfo().nativeLibraryDir, true);
     } catch (Exception e) {
       // nothing to do
     }
@@ -47,10 +47,10 @@ public class AndroidPlugin implements FlutterPlugin, MethodCallHandler, Activity
 
   @Override
   public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-    mActivity = activityPluginBinding.getActivity();
+    PythonActivity.mActivity = activityPluginBinding.getActivity();
     try {
-      Os.setenv(MAIN_ACTIVITY_HOST_CLASS_NAME, this.getClass().getCanonicalName(), true);
-      Os.setenv(MAIN_ACTIVITY_CLASS_NAME, mActivity.getClass().getCanonicalName(), true);
+      Os.setenv(MAIN_ACTIVITY_HOST_CLASS_NAME, PythonActivity.class.getCanonicalName(), true);
+      Os.setenv(MAIN_ACTIVITY_CLASS_NAME, PythonActivity.mActivity.getClass().getCanonicalName(), true);
     } catch (Exception e) {
       // nothing to do
     }
